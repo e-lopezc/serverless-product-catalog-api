3e11c16225c9858eb7f98f1fe6bab6b585b260e5
Elias Lopez <eliasjavier@gmail.com>
2025-05-27 20:23:46 +0100

    adding files for lambda module

---
+++ modules/lambda/main.tf
@@ -0,0 +1,20 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src/${var.item_name}_crud"
+  output_path = "${path.module}/src/${var.item_name}_crud/${var.item_name}_crud_function.zip"
+}
+
+
+resource "aws_lambda_function" "crud_function" {
+  filename      = data.archive_file.lambda_zip.output_path
+  handler       = "index.lambda_handler"
+  function_name = var.function_name
+  role          = var.execution_role_arn
+  runtime       = var.runtime
+  timeout       = var.timeout
+  environment {
+    variables = {
+      DYNAMO_TABLE_NAME = "${var.dynamodb_table_name}"
+    }
+  }
+}
